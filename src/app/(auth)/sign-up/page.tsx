f185bbad5a394460df7179ef0abe25cbe1eb4fb6
Lymmichael <129142623+Lymmichael@users.noreply.github.com>
2024-03-01 14:18:39 +0800

    done: sign-up page finished(email verification done)

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -7,9 +7,11 @@ import { cn } from "@/lib/utils"
 import { ArrowRight } from "lucide-react"
 import {useForm} from "react-hook-form"
 import {zodResolver} from "@hookform/resolvers/zod"
-import {z} from "zod"
+import {ZodError, z} from "zod"
 import {TAuthCredentialsValidator,AuthCredentialsValidator} from '@/lib/validators/account-credentials-validators'
 import { trpc } from "@/trpc/client"
+import {toast} from 'sonner'
+import { useRouter } from "next/navigation"
 
 const Page=()=>{
 
@@ -25,7 +27,28 @@ const Page=()=>{
 
     // console.log(data)
 
-    const {mutate, isLoading}=trpc.auth.createPayloadUser.useMutation({})
+    const router=useRouter()
+
+    const {mutate, isLoading}=trpc.auth.createPayloadUser.useMutation({
+        onError:(err)=>{
+            if(err.data?.code==="CONFLICT"){
+                toast.error("This email is already in use. Sign in instead")
+                //called in layout <Toaster />
+                return 
+            }
+
+            if(err instanceof ZodError){
+                toast.error(err.issues[0].message)
+                return
+            }
+
+            toast.error('Something goes wrong.')
+        },
+        onSuccess:({sentToEmail})=>{
+            toast.success(`Verification email sent to ${sentToEmail}`)
+            router.push('/verify-email?to='+sentToEmail)
+        }
+    })
 
     function onSubmit({email, password}:TAuthCredentialsValidator){
         mutate({email,password})
@@ -62,6 +85,9 @@ const Page=()=>{
                                 })}
                                 placeholder='you@example.com'
                             />
+                            {errors?.email && <p className='text-sm text-red-500'>
+                                {errors.email.message}
+                                </p>}
                         </div>
 
                         <div className='grid gap-1 py-2'>
@@ -74,6 +100,9 @@ const Page=()=>{
                                 })}
                                 placeholder='password'
                             />
+                            {errors?.password && <p className='text-sm text-red-500'>
+                                {errors.password.message}
+                                </p>}
                         </div>
                     
                         <Button >Sign up</Button>
