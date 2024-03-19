ca21e7c331bac1bee747f2f8470fa738d5ce745f
lymMichael <lymmichaellawlym@gmail.com>
2024-03-20 06:24:35 +0800

    add: photos and snacks categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,11 +28,10 @@ export default function Home() {
     <MaxWidthWrapper>
       <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
         <h1 className='text-4xl font-bold tracking-tight text-grey-900 sm:text-6xl'>
-          Your marketplace for high-quality{' '}
-        <span className='text-blue-600'>
           Snacks
+        <span className='text-blue-600'>
+          Express
         </span>
-        .
         </h1>
         <p className='mt-6 text-lg max-w-prose text-muted-foreground'>
           Weclome to SnackExpress. Every asset on our platform is verified by us!
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,25 +1,25 @@
 export const PRODUCT_CATEGORIES=[
-    {
-        label:"UI Kits",
-        value:"ui_kits" as const,
-        featured:[
-            {
-                name:"Editor picks",
-                href:'#',
-                imageSrc:'/nav/ui-kits/mixed.jpg'
-            },
-            {
-                name:"New Arrivals",
-                href:'#',
-                imageSrc:'/nav/ui-kits/blue.jpg'
-            },
-            {
-                name:"Bestsellers",
-                href:'#',
-                imageSrc:'/nav/ui-kits/purple.jpg'
-            }
-        ]
-    },
+    // {
+    //     label:"UI Kits",
+    //     value:"ui_kits" as const,
+    //     featured:[
+    //         {
+    //             name:"Editor picks",
+    //             href:'#',
+    //             imageSrc:'/nav/ui-kits/mixed.jpg'
+    //         },
+    //         {
+    //             name:"New Arrivals",
+    //             href:'#',
+    //             imageSrc:'/nav/ui-kits/blue.jpg'
+    //         },
+    //         {
+    //             name:"Bestsellers",
+    //             href:'#',
+    //             imageSrc:'/nav/ui-kits/purple.jpg'
+    //         }
+    //     ]
+    // },
     {
         label:"Chips and Crisps",
         value:"chips_and_crisps" as const,
